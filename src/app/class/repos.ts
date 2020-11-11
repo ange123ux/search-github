2a6988606012cc3dc347b443c6e7fec076b61447
Ange Uwimana <uuwange@gmail.com>
2020-11-11 18:19:34 +0200

    Made the set up for the github

---
+++ src/app/class/repos.ts
@@ -0,0 +1,8 @@
+export class Repos {
+
+    constructor(public name:string, public html_url:string){
+        this.name = name;
+        this.html_url = html_url; 
+    }
+    
+}
---
+++ src/app/class/users.ts
@@ -0,0 +1,11 @@
+export class Users {
+    
+    constructor (public login:string,public name:string,public avatar_url:string,public html_url: string){
+
+        this.name=name;
+        this.login = login;
+        this.avatar_url = avatar_url;
+        this.html_url = html_url;
+
+    }
+}
